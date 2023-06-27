401885be89ec369a1a5f81d3c6ff34a6be25915b
jd0097 <dh.react123@gmail.com>
2023-06-27 21:32:36 +0900

    feat: diary 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNoteSticky } from '@fortawesome/free-solid-svg-icons';
+import { faClipboardList } from '@fortawesome/free-solid-svg-icons';
+
 
 const Main = () => {
   return (
@@ -6,11 +10,11 @@ const Main = () => {
       <h1 className="title">HOME</h1>
       <div className="banner_box">
         <div className="banner">
-          <h1>MY NOTE</h1>
+          <h1><FontAwesomeIcon icon={faNoteSticky}/> MY NOTE</h1>
           <span>오늘 생각과 아이디어를 자유롭게 적어보세요</span>
         </div>
         <div className="banner">
-          <h1>STUDY-PLAN</h1>
+          <h1><FontAwesomeIcon icon={faClipboardList}/> STUDY-PLAN</h1>
           <span>계획하신 스터디 플랜이 있으신가요?</span>
         </div>
       </div>
