98ea03f11c51009926e00357babbf236b1f11a47
LSYbuilds <builddav2009@gmail.com>
2023-06-27 12:41:51 +0900

    feat : scss 작업중

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import "./scss/layout.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Bg from "./components/Bg";
+import Diary from "./pages/Diary";
+import Intro from "./pages/Intro";
+import Main from "./pages/Main";
+import Mypages from "./pages/Mypages";
+import Note from "./pages/Note";
+import NoteWrite from "./pages/NoteWrite";
+import NotFound from "./pages/NotFound";
+import Write from "./pages/write";
+import DayList from "./pages/DayList";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-   StudyHub
+      <Bg />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Intro />}></Route>
+        <Route path="/main" element={<Main />}></Route>
+        <Route path="/note" element={<Note />}></Route>
+        <Route path="/noteWrite" element={<NoteWrite />}></Route>
+        <Route path="/mypages" element={<Mypages />}></Route>
+        <Route path="/Diary" element={<Diary />}></Route>
+        <Route path="/write" element={<Write />}></Route>
+        <Route path="/dayList" element={<DayList />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/NoteWrite.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteWrite = () => {
+  return (
+    <div>NoteWrite</div>
+  )
+}
+
+export default NoteWrite
