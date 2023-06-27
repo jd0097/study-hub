25db210705c5f48454da13bd3251b5344c6f2e02
LSYbuilds <builddav2009@gmail.com>
2023-06-27 19:17:49 +0900

    feat : MAIN SCSS 작업중

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <div className="home_wrap">
+      <h1 className="title">HOME</h1>
+      <div className="banner_box">
+        <div className="banner">
+          <h1>MY NOTE</h1>
+          <span>오늘 생각과 아이디어를 자유롭게 적어보세요</span>
+        </div>
+        <div className="banner">
+          <h1>STUDY-PLAN</h1>
+          <span>계획하신 스터디 플랜이 있으신가요?</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
