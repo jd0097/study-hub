abfe22d3bb610084db19c24faa9f6431b04189ff
LSYbuilds <builddav2009@gmail.com>
2023-06-29 09:51:39 +0900

    feat : 노트,마이페이지 SCSS 작업

--- src/pages/Note.js
+++ src/pages/Note.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const Note = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/notewrite");
+  };
   return (
     <div className="note_wrap">
-      <div className="title_box">
-        <span>
-          작성하신 메모가 없습니다.
-          <br />
-          메모를 작성해보세요
-        </span>
-        <button className="memo_button">메모작성하기</button>
+      <h1 className="title">메모</h1>
+      <div className="note_wrap_inner">
+        <div className="title_box">
+          <span>
+            작성하신 메모가 없습니다.
+            <br />
+            메모를 작성해보세요
+          </span>
+          <button className="memo_button" onClick={handleClick}>
+            메모작성하기
+          </button>
+        </div>
       </div>
     </div>
   );
